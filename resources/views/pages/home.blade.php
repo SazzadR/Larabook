6abb27ae882c667283c2810224329465bdbe80b6
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2016-11-08 23:46:41 +0600

    Basic templating

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.default')
+
+@section('content')
+    <div class="jumbotron">
+        <h1>Welcome to Larabook!</h1>
+        <p>Welcome to the premier place to talk about Laravel. Why don't you sighup to see what all the fuss is about?</p>
+        <p>
+            <a class="btn btn-lg btn-primary" href="#" role="button">Sign Up! &raquo;</a>
+        </p>
+    </div>
+@stop
